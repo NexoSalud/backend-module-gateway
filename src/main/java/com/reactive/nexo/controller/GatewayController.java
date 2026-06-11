10b789e65595372182ed8e17d172eea877f87b99
Marvin Banguero <malejandrobanguero@gmail.com>
2026-06-11 09:41:22 -0500

    feat: add SIAU service route to gateway

--- src/main/java/com/reactive/nexo/controller/GatewayController.java
+++ src/main/java/com/reactive/nexo/controller/GatewayController.java
@@ -40,6 +40,7 @@ public class GatewayController {
     private String urlHistory = System.getenv().getOrDefault("HISTORY_TEMPLATE_SERVICE_URL", "http://localhost:8085");
     private String urlConvenios = System.getenv().getOrDefault("CONVENIOS_SERVICE_URL", "http://localhost:8086");
     private String urlBilling = System.getenv().getOrDefault("BILLING_SERVICE_URL", "http://localhost:8087");
+    private String urlSiau = System.getenv().getOrDefault("SIAU_SERVICE_URL", "http://localhost:8088");
 
     private final Map<String, WebClient>  webClients;
 
@@ -55,6 +56,7 @@ public class GatewayController {
         logger.info("URL Appointments: {}", urlAppointments);
         logger.info("URL Convenios: {}", urlConvenios);
         logger.info("URL Billing: {}", urlBilling);
+        logger.info("URL SIAU: {}", urlSiau);
         
         webClients.put("/api/v1/users", WebClient.create(urlUsers));
         webClients.put("/api/v1/employees", WebClient.create(urlEmployees));
@@ -72,7 +74,9 @@ public class GatewayController {
         webClients.put("/api/v1/headquarters", webClients.get("/api/v1/employees"));
         webClients.put("/api/v1/service-types", webClients.get("/api/v1/schedule"));
         webClients.put("/api/v1/specialties", webClients.get("/api/v1/schedule"));
-        webClients.put("/api/v1/tracking", webClients.get("/api/v1/employees"));*/
+        webClients.put("/api/v1/tracking", webClients.get("/api/v1/employees"));
+        // Rutas SIAU - PQRSDF
+        webClients.put("/api/v1/siau", WebClient.create(urlSiau));
      
     }
     private WebClient getWebClient(String path){
